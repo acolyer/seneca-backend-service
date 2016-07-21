ddb2c43489600b309a6bed4047689c33b0b96a37
Adrian Colyer <adrian@aspectprogrammer.org>
2016-07-21 08:32:33 +0100

    experimental changes for new template style

---
+++ project/role_name.js
@@ -0,0 +1,8 @@
+// ${description}
+module.exports = function role_name(options) {
+
+  this.add({role: 'role_name', cmd: 'ping'}, function ping(msg,respond)) {
+    respond(null, {answer: 'pong'})
+  }
+
+}
